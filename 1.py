0b30485a2a3db0b30fa83234ebe59ed9b3a5b87b
sajjad-jafri <mohdsajjad26@gmail.com>
2024-06-20 14:12:04 +0530

    All Pages added

--- 1.py
+++ 1.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
