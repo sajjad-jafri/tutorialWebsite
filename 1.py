448a7662501dd73ae2100683907bcad519ca5de3
sajjad-jafri <mohdsajjad26@gmail.com>
2024-06-21 16:08:43 +0530

    python input output page completed

--- 1.py
+++ 1.py
@@ -23,13 +23,17 @@ def contact():
 def python():
     return render_template("python/python.html")
 
-@app.route("/courses/pythonIntro")
+@app.route("/courses/python/Introduction")
 def pythonIntro():
     return render_template("python/python_intro.html")
 
-@app.route("/courses/pythonInputOutput")
+@app.route("/courses/python/Input-Output")
 def pythonInputOutput():
     return render_template("python/inpOut.html")
+
+@app.route("/courses/Python/Data-Types")
+def dataTypes():
+    return render_template("python/dataTypes.html")
 
 
 if __name__ == "__main__":
