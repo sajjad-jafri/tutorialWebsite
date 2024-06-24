d661ade4f0240286ca9d295053ee540fcfc23851
sajjad-jafri <mohdsajjad26@gmail.com>
2024-06-24 11:42:17 +0530

    courses folder added

--- 1.py
+++ 1.py
@@ -2,54 +2,60 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# Home Page
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/courses")
+# Courses Page
+@app.route("/courses/")
 def courses():
     return render_template("courses.html")
 
-@app.route("/about")
+# About Page
+@app.route("/about/")
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+# Contact Page
+@app.route("/contact/")
 def contact():
     return render_template("contact.html")
 
-@app.route("/courses/python")
+
+@app.route("/courses/python/")
 def python():
-    return render_template("python/python.html")
+    return render_template("courses/python/python.html")
 
-@app.route("/courses/python/Introduction")
+@app.route("/courses/python/Introduction/")
 def pythonIntro():
-    return render_template("python/python_intro.html")
+    return render_template("courses/python/python_intro.html")
 
-@app.route("/courses/python/Input-Output")
+@app.route("/courses/python/Input-Output/")
 def pythonInputOutput():
-    return render_template("python/inpOut.html")
+    return render_template("courses/python/inpOut.html")
 
-@app.route("/courses/python/Data-Types")
+@app.route("/courses/python/Data-Types/")
 def pythonDataTypes():
-    return render_template("python/dataTypes.html")
+    return render_template("courses/python/dataTypes.html")
 
-@app.route("/courses/python/Control-Flow-If-Statements")
+@app.route("/courses/python/Control-Flow-If-Statements/")
 def pythonIfStatements():
-    return render_template("python/ifStatements.html")
+    return render_template("courses/python/ifStatements.html")
 
-@app.route("/courses/python/For-Loop")
+@app.route("/courses/python/For-Loop/")
 def pythonForLoop():
-    return render_template("python/forLoop.html")
+    return render_template("courses/python/forLoop.html")
 
-@app.route("/courses/python/While-Loop")
+@app.route("/courses/python/While-Loop/")
 def pythonWhileLoop():
-    return render_template("python/whileLoop.html")
+    return render_template("courses/python/whileLoop.html")
 
-@app.route("/courses/python/Transfer-Statements")
+@app.route("/courses/python/Transfer-Statements/")
 def pythonTransferStatements():
-    return render_template("python/transferStatements.html")
+    return render_template("courses/python/transferStatements.html")
 
 
 if __name__ == "__main__":
