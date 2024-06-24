225b3e5a85b7b74a0690363d6b62fb5f03c6d76c
sajjad-jafri <mohdsajjad26@gmail.com>
2024-06-24 15:28:00 +0530

    Dynamic routing

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 
 # Home Page
-@app.route("/home")
+@app.route("/home/")
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 # Courses Page
 @app.route("/courses/")
@@ -24,11 +25,23 @@ def about():
 def contact():
     return render_template("contact.html")
 
+# Dynamic route for courses and chapters
+@app.route("/courses/<course_name>/")
+def display_course(course_name):
+    return render_template(f"courses/{course_name}.html")
 
+# Dynamic route for chapters within a course
+@app.route("/courses/<course_name>/<chapter_name>/")
+def display_chapter(course_name, chapter_name):
+    active_chapter = chapter_name
+    return render_template(f"courses/{course_name}/{chapter_name}.html", active_chapter = active_chapter)
+
+'''
 @app.route("/courses/python/")
 def python():
     return render_template("courses/python/python.html")
-
+'''
+'''
 @app.route("/courses/python/Introduction/")
 def pythonIntro():
     return render_template("courses/python/python_intro.html")
@@ -56,7 +69,7 @@ def pythonWhileLoop():
 @app.route("/courses/python/Transfer-Statements/")
 def pythonTransferStatements():
     return render_template("courses/python/transferStatements.html")
-
+'''
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
